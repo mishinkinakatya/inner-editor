2c3324ea08aa45f4cfc7ee9ad00fb8bbe25daff4
mishinkinakatya <mishinkinakatya@gmail.com>
2020-09-29 00:07:49 +0500

    setup project

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Example from "./components/example";
+
+ReactDOM.render(
+    <Example />,
+    document.querySelector(`#root`)
+)
